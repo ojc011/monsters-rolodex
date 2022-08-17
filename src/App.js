9e246c2b4e7f13d3ce2df32f2a4691a5b52d0931
Oliver C <ojc011@yahoo.com>
2022-08-17 11:55:05 -0500

    Moved anonymous onSearchChange function into the method and created variables pointing to them to imrpove syntax and readability. Moving function to method will also improve performance bc it will only be rendered when it is called rather than every time the app is rendered

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // Entire app is encapsulated inside this component //
   constructor() {
     // Constructor is always ran first in javascript within a class //
     super();
@@ -32,11 +33,20 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -45,13 +55,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="Search monsters"
-          onChange={(event) => {
-            console.log(event.target.value);
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {
           filteredMonsters.map((monster) => {
